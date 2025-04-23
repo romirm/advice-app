13860cfc6c0ec68f3948140129ece2dc99eaa8f7
clementkubica <clement.kubica@hotmail.com>
2025-04-23 14:06:18 -0500

    Added text input to home page (basic initial layout)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import InputBox from "../components/InputBox"
+
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Advice App</h1>
-      <p>This is the homepage.</p>
+    <div className="flex flex-col items-center justify-center gap-8">
+      <h1 className="text-3xl font-bold">Welcome to the Advice App</h1>
+      <InputBox />
     </div>
   )
 }
