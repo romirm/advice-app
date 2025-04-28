4e4a9480caa600b3e083906d255b441c7aaa27d7
Romir Mohan <romirmohan2028@u.northwestern.edu>
2025-04-28 11:54:44 -0500

    changed name + styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import InputBox from "../components/InputBox"
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-8">
-      <h1 className="text-3xl font-bold">Welcome to the Advice App</h1>
+      <h1 className="text-3xl font-bold">Welcome to Aptly!</h1>
       <InputBox />
     </div>
   )
